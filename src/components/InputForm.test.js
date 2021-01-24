fe661ef12a52d08112f965238d94b0bdb9fdc24e
Jan Krivohlavek <krajvy@gmail.com>
2021-01-24 07:38:41 +0100

    Tests: Fix InputForm test with NCState

--- src/components/InputForm.test.js
+++ src/components/InputForm.test.js
@@ -1,30 +1,38 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
+import NCState from './../context/NCState';
+
 import InputForm from './InputForm';
 
 /* globals test, expect */
 
+const renderForm = () => render(
+  <NCState>
+    <InputForm />
+  </NCState>,
+);
+
 test('Form should contain Input legend', () => {
-  const { getByText } = render(<InputForm />);
+  const { getByText } = renderForm();
   const legend = getByText('Input');
   expect(legend).toBeInTheDocument();
 });
 
 test('Form should contain Coordinates input', () => {
-  const { getByLabelText } = render(<InputForm />);
+  const { getByLabelText } = renderForm();
   const label = getByLabelText('Your coordinates:');
   expect(label).toBeInTheDocument();
 });
 
 test('Form should contain File input', () => {
-  const { getByLabelText } = render(<InputForm />);
+  const { getByLabelText } = renderForm();
   const label = getByLabelText('Input file:');
   expect(label).toBeInTheDocument();
 });
 
 test('Form should contain Submit butotn', () => {
-  const { getByText } = render(<InputForm />);
+  const { getByText } = renderForm();
   const button = getByText('Read data');
   expect(button).toBeInTheDocument();
 });
