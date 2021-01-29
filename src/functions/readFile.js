c140d5cd9594ff97c68e8cc0730471c55eb61731
Jan Krivohlavek <krajvy@gmail.com>
2021-01-29 08:31:12 +0100

    Do not read file data when no file is passed

--- src/functions/readFile.js
+++ src/functions/readFile.js
@@ -1,17 +1,17 @@
 const readFile = fileList => new Promise((resolve, reject) => {
-  // check if we have FileReader global object
   if (!FileReader) {
     reject(new Error('FileReader does not exists!'));
   }
 
-  // just use first file
+  if (fileList.length < 1) {
+    reject(new Error('No file to read passed!'));
+  }
+
   const file = fileList[0];
-  // allow to read only txt files
   if (!file.type.match('text.*')) {
     reject(new Error('Cannot parse this file as text!'));
   }
 
-  // prepare file reading
   const fileReader = new FileReader();
   fileReader.onerror = reject;
 
--- src/functions/readFile.test.js
+++ src/functions/readFile.test.js
@@ -2,6 +2,14 @@ import readFile from './readFile';
 
 /* globals test, expect */
 
+test('Should throw an error, when no file is passed', async () => {
+  const fileList = [];
+  await readFile(fileList)
+    .catch(error => {
+      expect(error).toEqual(Error('No file to read passed!'));
+    });
+});
+
 test('Should throw an error, when passed file is not txt', async () => {
   const fileList = [
     {
