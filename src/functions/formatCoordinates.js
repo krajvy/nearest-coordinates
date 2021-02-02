98abb0a4b1ebaf356c9ed0b2785e654d3a9fb78b
Jan Krivohlavek <krajvy@gmail.com>
2021-02-02 08:37:11 +0100

    Move precisionRound to math folder

--- src/functions/formatCoordinates.js
+++ src/functions/formatCoordinates.js
@@ -1,4 +1,4 @@
-import precisionRound from './precisionRound';
+import precisionRound from './math/precisionRound';
 
 const formatCoordinates = (latitude, longitude) => {
   return precisionRound(latitude, 5) + ',' + precisionRound(longitude, 5);
