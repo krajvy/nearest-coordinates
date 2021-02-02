10530860c7720d4efd3eee2ef4e5ddbd4227682e
Jan Krivohlavek <krajvy@gmail.com>
2021-02-02 08:45:17 +0100

    Move format functions to separate folder

--- src/components/OutputTable.jsx
+++ src/components/OutputTable.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import formatCoordinates from './../functions/formatCoordinates';
+import formatCoordinates from '../functions/format/formatCoordinates';
 
 const OutputTable = props => {
   return (
