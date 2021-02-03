8b2da192dbc26d7354ec1db9d101ae4c927b1bc6
Jan Krivohlavek <krajvy@gmail.com>
2021-02-03 06:38:04 +0100

    Check passed coordinates to be correct

--- src/state/middleware.js
+++ src/state/middleware.js
@@ -2,15 +2,19 @@ import dataComplete from './actions/dataComplete';
 import preprocessData from './actions/preprocessData';
 import readFile from '../functions/file/readFile';
 import preprocessFile from '../functions/file/preprocessFile';
+import parseCoordinates from '../functions/parseCoordinates';
+import issetCoordinates from '../functions/issetCoordinates';
 
 import { PREPROCESSDATA, READDATA } from './constants';
+
+const checkCoordinates = coordinates => issetCoordinates(parseCoordinates(coordinates));
 
 const middleware = store => next => action => {
   const state = store.getState();
 
   switch (action.type) {
   case READDATA:
-    if (!state.coordIn || !state.fileIn) {
+    if (!state.coordIn || !state.fileIn || !checkCoordinates(state.coordIn)) {
       console.error('Input fields not filled properly!');
       break;
     }
