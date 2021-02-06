8237791738abcb467a8170e0ec956cbba0726577
Jan Krivohlavek <krajvy@gmail.com>
2021-02-06 07:06:54 +0100

    Draw an arrow of azimuth in output table

--- src/components/OutputTable.jsx
+++ src/components/OutputTable.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
 import formatCoordinates from '../functions/format/formatCoordinates';
+
+const Arrow = styled.span`
+  position: absolute;
+  transform: rotate(${props => props.azimuth - 90}deg);
+`;
+const Azimuth = styled.span`
+  margin-left: 1.2em;
+`;
 
 const OutputTable = props => {
   return (
@@ -22,7 +31,10 @@ const OutputTable = props => {
             <tr key={index}>
               <td className="coordinates">{formatCoordinates(row.latitude, row.longitude)}</td>
               <td className="distance">{row.distance} km</td>
-              <td className="azimuth">{row.azimuth}</td>
+              <td className="azimuth">
+                <Arrow azimuth={row.azimuth}>&#10140;</Arrow>
+                <Azimuth>{row.azimuth}</Azimuth>
+              </td>
               <td className="description">{row.description}</td>
               <td className="map"></td>
             </tr>
