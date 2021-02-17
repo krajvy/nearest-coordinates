4183ed3b31e6aa6c5e3916beb86a8b0df886f18e
Jan Krivohlavek <krajvy@gmail.com>
2021-02-17 07:00:06 +0100

    Restyle output table to be more readable

--- src/components/OutputTable.jsx
+++ src/components/OutputTable.jsx
@@ -17,43 +17,76 @@ const Azimuth = styled.span`
   margin-left: 1.2em;
 `;
 
+const Table = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+  th, td {
+    border: 1px solid black;
+    padding-left: 0.3em;
+    padding-right: 0.3em;
+  }
+`;
+
+const TdCoordinates = styled.td`
+  text-align: center;
+  width: 10em;
+`;
+
+const TdDistance = styled.td`
+  text-align: right;
+  width: 5em;
+`;
+
+const TdAzimuth = styled.td`
+  text-align: center;
+  width: 4em;
+`;
+
+const TdMap = styled.td`
+  text-align: center;
+  width: 2em;
+`;
+
 const OutputTable = props => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Coordinates</th>
-          <th>Distance</th>
-          <th>Azimuth</th>
-          <th>Description</th>
-          <th>Map</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.data.map((row, index) => {
-          return (
-            <tr key={index}>
-              <td className="coordinates">{formatCoordinates(row.latitude, row.longitude)}</td>
-              <td className="distance">
-                {row.distance > 50 ? parseInt(row.distance, 10) : row.distance} km
-              </td>
-              <td className="azimuth">
-                <Arrow azimuth={row.azimuth}>&#10140;</Arrow>
-                <Azimuth>{row.azimuth}</Azimuth>
-              </td>
-              <td className="description">{row.description}</td>
-              <td className="map">
-                <InputCheckbox
-                  name={'map-' + index}
-                  onChange={e => { props.setDisplayOnMap(e, index); }}
-                  checked={row.displayOnMap}
-                />
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <fieldset>
+      <legend>Output table</legend>
+      <Table>
+        <thead>
+          <tr>
+            <th>Coordinates</th>
+            <th>Distance</th>
+            <th>Azimuth</th>
+            <th>Description</th>
+            <th>Map</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.data.map((row, index) => {
+            return (
+              <tr key={index}>
+                <TdCoordinates>{formatCoordinates(row.latitude, row.longitude)}</TdCoordinates>
+                <TdDistance>
+                  {row.distance > 50 ? parseInt(row.distance, 10) : row.distance} km
+                </TdDistance>
+                <TdAzimuth>
+                  <Arrow azimuth={row.azimuth}>&#10140;</Arrow>
+                  <Azimuth>{row.azimuth.toString().padStart(3, 0)}</Azimuth>
+                </TdAzimuth>
+                <td>{row.description}</td>
+                <TdMap>
+                  <InputCheckbox
+                    name={'map-' + index}
+                    onChange={e => { props.setDisplayOnMap(e, index); }}
+                    checked={row.displayOnMap}
+                  />
+                </TdMap>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </fieldset>
   );
 };
 
