9de48b5f7eb24f3f4b8235523bfaa92fe14e1af2
Jan Krivohlavek <krajvy@gmail.com>
2021-02-27 07:52:23 +0100

    Add help component with hints how to use webapp

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import InputForm from './components/InputForm';
 import MapForm from './components/MapForm';
 import OutputTable from './components/OutputTable';
+import Help from './components/Help';
 
 const App = () => {
   return (
     <div className="App">
+      <Help />
       <InputForm />
       <MapForm />
       <OutputTable />
