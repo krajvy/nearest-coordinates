cd1f2f70623d025e12634858cdf62515ca1f81aa
Jan Krivohlavek <krajvy@gmail.com>
2021-11-06 07:37:13 +0100

    feat: Automatic deploy GitHub workflow scripts

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    rules: {
-        'body-case': [1, 'always', 'sentence-case'],
-        'scope-case': [1, 'always', 'sentence-case'],
-        'subject-case': [1, 'always', 'sentence-case'],
-    },
+  rules: {
+    'body-case': [1, 'always', 'sentence-case'],
+    'scope-case': [1, 'always', 'sentence-case'],
+    'subject-case': [1, 'always', 'sentence-case'],
+  },
 };
