471d741562480ffa8707b8ae441d61ac5e1705b6
Jan Krivohlavek <krajvy@gmail.com>
2022-06-01 15:21:07 +0200

    Update packages and dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -10,13 +10,15 @@ import { Provider } from 'react-redux';
 import store from './state/store';
 window.store = store;
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </Provider>,
-  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
